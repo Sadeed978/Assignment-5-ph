75d3ef4356e58389e9b30ca576a26653734e2d03
Sadeed978 <sadeedahmed681@gmail.com>
2025-08-29 18:32:09 +0600

    5th commit

--- js/function.js
+++ js/function.js
@@ -5,6 +5,11 @@ document.getElementById('clear-btn').addEventListener('click', function(){
 })
 document.getElementById('call-btn').addEventListener('click', function(){
     
+    const serviceSelect = document.getElementsById('service-select')[0];
+    const selectedOption = serviceSelect.options[serviceSelect.selectedIndex];
+    const serviceName = selectedOption.text;
+    const serviceNumber = selectedOption.value;
+
     const callHistoryContainer = document.getElementById('call-history');
     const newEntry = document.createElement('div');
     newEntry.classList.add('mb-4', 'p-4', 'border', 'rounded', 'bg-gray-100');
@@ -17,3 +22,44 @@ document.getElementById('call-btn').addEventListener('click', function(){
     // Append the new entry to the call history container
     callHistoryContainer.appendChild(newEntry);
 });
+    
+// Add event listener to heart buttons
+document.querySelectorAll('.heart-btn').forEach(button => {
+    button.addEventListener('click', function() {
+        document.getElementById("heart-count").innerText = parseInt(document.getElementById("heart-count").innerText) + 1 ;
+        
+    });
+});
+
+document.querySelectorAll('.btn').forEach(button => {
+    button.addEventListener('click', function() {
+        document.getElementById("copy-count").innerText = parseInt(document.getElementById("copy-count").innerText) + 1 ;
+    });
+});
+
+var copyButtons = document.getElementsByClassName('btn');
+for (var i = 0; i < copyButtons.length; i++) {
+    copyButtons[i].addEventListener('click', function() {
+      
+        var card = this;
+        while (card && !card.classList.contains('card')) {
+            card = card.parentElement;
+        }
+       
+        var headings = card.getElementsByTagName('h1');
+        var textToCopy = headings[2] ? headings[2].textContent : headings[1].textContent;
+
+       
+        var tempInput = document.createElement('input');
+        tempInput.value = textToCopy;
+        document.body.appendChild(tempInput);
+        tempInput.select();
+       
+        document.body.removeChild(tempInput);
+
+        
+        alert('Copied: ' + textToCopy);
+    });
+}
+
+
